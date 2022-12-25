4f8f597cb0a3507fcf6685cb7fd4d04df69dc1a6
Phi Thai <thaimanhphi@gmail.com>
2022-12-25 23:48:00 +0700

    Add details activity, change theme system

---
+++ app/src/main/java/com/team5/besthouse/DetailActivity.java
@@ -0,0 +1,26 @@
+package com.team5.besthouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        LinearLayout bedroom = findViewById(R.id.details_bedroom);
+        LinearLayout other = findViewById(R.id.details_other);
+
+        ImageView featureImage = bedroom.findViewById(R.id.feature_image);
+        featureImage.setImageResource(R.drawable.ic_outline_single_bed_24);
+
+        ImageView featureOther = other.findViewById(R.id.feature_image);
+        featureOther.setImageResource(R.drawable.ic_outline_done_outline_24);
+    }
+}
