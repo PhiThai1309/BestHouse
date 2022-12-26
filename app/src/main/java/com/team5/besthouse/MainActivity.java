a2bdd55e4f12d7697acac6d478dd938626163110
Phi Thai <thaimanhphi@gmail.com>
2022-12-27 00:26:12 +0700

    Implement fragment

--- app/src/main/java/com/team5/besthouse/MainActivity.java
+++ app/src/main/java/com/team5/besthouse/MainActivity.java
@@ -3,22 +3,23 @@ package com.team5.besthouse;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toolbar;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        NavigationBarView navigationView;
 
 //        LinearLayout navbar = findViewById(R.id.main_navbar);
 //
@@ -29,6 +30,39 @@ public class MainActivity extends AppCompatActivity {
 //        home.setImageResource(R.drawable.ic_round_home_24);
 //        home.setColorFilter(getResources().getColor(R.color.md_theme_onPrimaryContainer));
 
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnItemSelectedListener(selectedListener);
+
+        // When we open the application first
+        // time the fragment should be shown to the user
+        // in this case it is home fragment
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment, "");
+        fragmentTransaction.commit();
 
     }
+
+    private final NavigationBarView.OnItemSelectedListener selectedListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+
+                case R.id.main:
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, fragment, "");
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.account:
+                    AccountFragment fragment2 = new AccountFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.content, fragment2, "");
+                    fragmentTransaction2.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 }
