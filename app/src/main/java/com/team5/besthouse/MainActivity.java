ddebd38b26db5810f4cd6f674ddfc0dabef93e8b
Phi Thai <thaimanhphi@gmail.com>
2022-12-26 22:34:31 +0700

    Finalize UI design for home screen

--- app/src/main/java/com/team5/besthouse/MainActivity.java
+++ app/src/main/java/com/team5/besthouse/MainActivity.java
@@ -1,8 +1,17 @@
 package com.team5.besthouse;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        LinearLayout navbar = findViewById(R.id.main_navbar);
+//
+//        ImageView home = navbar.findViewById(R.id.home);
+//        ImageView search = navbar.findViewById(R.id.search);
+//        ImageView profile = navbar.findViewById(R.id.profile);
+//
+//        home.setImageResource(R.drawable.ic_round_home_24);
+//        home.setColorFilter(getResources().getColor(R.color.md_theme_onPrimaryContainer));
+
+
     }
 }
