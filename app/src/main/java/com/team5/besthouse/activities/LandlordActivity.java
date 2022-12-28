0ad06ea6f76231af7b1b2e59f15f241a5ea7ee3a
ngcmnh <s3907086@rmit.edu.vn>
2022-12-28 16:16:46 +0700

    add landlord layout

---
+++ app/src/main/java/com/team5/besthouse/activities/LandlordActivity.java
@@ -0,0 +1,15 @@
+package com.team5.besthouse.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.team5.besthouse.R;
+
+
+public class LandlordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landlord);
+    }
+}
