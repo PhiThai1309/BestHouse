d175385de0bdf272fe956a19f290477337b45e15
Phi Thai <thaimanhphi@gmail.com>
2023-01-04 16:19:10 +0700

    Changes in Details View UI

--- app/src/main/java/com/team5/besthouse/activities/DetailActivity.java
+++ app/src/main/java/com/team5/besthouse/activities/DetailActivity.java
@@ -19,6 +19,8 @@ import com.team5.besthouse.constants.UnchangedValues;
 import com.team5.besthouse.models.Property;
 import com.team5.besthouse.models.PropertyAddress;
 
+import org.w3c.dom.Text;
+
 public class DetailActivity extends AppCompatActivity {
     FirebaseFirestore db;
     Property property;
@@ -40,14 +42,28 @@ public class DetailActivity extends AppCompatActivity {
         //Set color to the navigation bar to match with the bottom navigation view
         getWindow().setNavigationBarColor(SurfaceColors.SURFACE_2.getColor(this));
 
+        //first feature
         LinearLayout bedroom = findViewById(R.id.details_bedroom);
+        ImageView featureBedroom = bedroom.findViewById(R.id.feature_image);
+        featureBedroom.setImageResource(R.drawable.ic_outline_single_bed_24);
+        TextView bedroomText = bedroom.findViewById(R.id.feature_text);
+        bedroomText.setText(property.getBedrooms() + " Bedrooms");
+
+        //Second feature
+        LinearLayout bathroom = findViewById(R.id.details_bathroom);
+        ImageView featureBathroom = bathroom.findViewById(R.id.feature_image);
+        featureBathroom.setImageResource(R.drawable.ic_outline_bathtub_24);
+        TextView bathroomText = bathroom.findViewById(R.id.feature_text);
+        bathroomText.setText(property.getBathrooms() + " Bathrooms");
+
+        //Last feature
         LinearLayout other = findViewById(R.id.details_other);
-
-        ImageView featureImage = bedroom.findViewById(R.id.feature_image);
-        featureImage.setImageResource(R.drawable.ic_outline_single_bed_24);
-
         ImageView featureOther = other.findViewById(R.id.feature_image);
-        featureOther.setImageResource(R.drawable.ic_outline_done_outline_24);
+        featureOther.setImageResource(R.drawable.ic_outline_square_foot_24);
+        TextView otherText = other.findViewById(R.id.feature_text);
+        otherText.setText((int) property.getArea() + "m^2 Square foot");
+
+        TextView desc = findViewById(R.id.details_desc);
 
         Button newPropertyButton = findViewById(R.id.createPropertyBtn);
 
@@ -69,6 +85,9 @@ public class DetailActivity extends AppCompatActivity {
 
             Toast.makeText(this, "New property added!", Toast.LENGTH_SHORT).show();
         });
+
+        TextView price = findViewById(R.id.details_price);
+        price.setText((int) property.getMonthlyPrice() + ".000 VND / Month");
 
         ImageView backBtn = findViewById(R.id.details_backBtn);
         backBtn.setOnClickListener(new View.OnClickListener() {
