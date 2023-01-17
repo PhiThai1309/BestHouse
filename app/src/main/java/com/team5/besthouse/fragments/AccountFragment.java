fdc63c5cb1a86dfba56197a1097bb59d0140bbef
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2023-01-17 12:55:18 +0700

    commit

--- app/src/main/java/com/team5/besthouse/fragments/AccountFragment.java
+++ app/src/main/java/com/team5/besthouse/fragments/AccountFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
